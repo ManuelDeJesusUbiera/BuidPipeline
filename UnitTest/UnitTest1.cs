c6916051f6941887d547924c89764630606a91fb
ManuelUbiera <20198501@itla.edu.do>
2021-07-12 14:23:32 -0400

    first commit

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PipelineAPP;
+using PipelineAPP.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void index()
+        {
+            PipelineController control = new PipelineController();
+            ViewResult c = control.Index() as ViewResult;
+        }
+    }
+}
